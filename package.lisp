08431909aaee456637b526e0cd6f1d4cb3278823
kagetora66 <sina.safari1313@gmail.com>
2026-04-30 19:02:19 +0330

    added remove paragraph package

--- package.lisp
+++ package.lisp
@@ -1,3 +1,3 @@
 (defpackage #:cl-docx
   (:use #:cl)
-  (:export #:get-all-paragraphs #:with-open-docx #:read-value #:write-value))
+  (:export #:get-all-paragraphs #:with-open-docx #:read-value #:write-value #:remove-paragraph))
