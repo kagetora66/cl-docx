f30b4c82220f88874c249518d63d5f821e63e412
kagetora66 <sina.safari1313@gmail.com>
2026-06-04 11:51:45 +0330

    export get-rows and get-cells

--- package.lisp
+++ package.lisp
@@ -1,3 +1,3 @@
 (defpackage #:cl-docx
   (:use #:cl)
-  (:export #:get-all-paragraphs #:with-open-docx #:read-value #:write-value #:remove-item #:get-all-texts #:get-all-tables))
+  (:export #:get-all-paragraphs #:with-open-docx #:read-value #:write-value #:remove-item #:get-all-texts #:get-all-tables #:get-rows #:get-cells))
